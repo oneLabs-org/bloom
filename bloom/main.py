72b44811b31bc865d9e7a38628345c2ca3c22be8
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-10-04 03:54:09 +0530

    Setup CORS allow all hosts * .

--- bloom/main.py
+++ bloom/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api.router.status import status_router
 from .api.router.auth import auth_github
 from .core.settings.logger import LogConfig
@@ -9,6 +10,15 @@ dictConfig(LogConfig())
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(status_router.router)
 app.include_router(auth_github.router)
